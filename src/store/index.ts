e4cc5ea0c458501caae097cf8a615326fafca597
CaoMeiYouRen <996881204@qq.com>
2021-06-26 20:14:18 +0800

    chore: add store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+    },
+    getters: {
+    },
+    // 只允许同步函数
+    mutations: {
+    },
+})
+
+export default store
